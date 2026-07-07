14c35723b31ffd87a3021be6dea450ad4a4db3a5
yanisa789 <yayalookwha@gmail.com>
2026-07-07 18:56:18 +0700

    x

---
+++ lecture2/test arithmeicoper.py
@@ -0,0 +1,19 @@
+x = 15
+y = 4
+# output: x+y =19
+print('x + y =' ,x+y)
+
+# output: x-y =11
+print('x - y =',x-y)
+
+# output: x * y = 60
+print('x * y =' ,x*y)
+
+# output: x / y = 3.75
+print('x / y =' ,x/y)
+
+print('x % y =' ,x%y)
+
+print('x // y =' ,x//y)
+
+print('x ** y =' ,x**y) 
---
+++ lecture2/test typeconvert.py
@@ -0,0 +1,11 @@
+# converting int to float
+age = 25
+height =  float(age) 
+
+# converting float to int
+height = 5.6
+age = int(height)
+
+# converting string to int
+num_str = "123"
+num = int(num_str)
