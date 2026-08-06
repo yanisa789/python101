8d5f8f2af5beff85d4c971154d865e74835bc5e7
yanisa789 <yayalookwha@gmail.com>
2026-08-06 08:51:19 +0700

    ....

---
+++ lecture5/func2.py
@@ -0,0 +1,12 @@
+#this program has two functions.
+#frist we define the main functions
+
+def main():
+    print("I have a message for you.")
+    message()
+#Next we define the message functions
+def message():
+    print("I am Yanisa,")
+    print("I love Noodles.")
+#call thr main functions
+main()
