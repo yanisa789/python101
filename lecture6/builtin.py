76595f86e09e99e3048044ec7b250413548dc3cc
yanisa789 <yayalookwha@gmail.com>
2026-08-13 10:00:22 +0700

    .....

---
+++ lecture6/builtin.py
@@ -0,0 +1,11 @@
+numbers = [4,2,9,1,5,6]
+length = len(numbers)
+print(f"Length of the list: {length}")
+total_sum = sum(numbers)
+print(f"Sum of all elements: {total_sum}")
+max_value = max(numbers)
+print(f"Maximum value: {max_value}")
+min_value = min(numbers)
+print(f"Minimu value: {min_value}")
+sorted_numbers = sorted(numbers)
+print(f"Sorted list: {sorted_numbers}")
