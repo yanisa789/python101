7f7535fec587fda90d76f9ebb903c63fdbb67182
yanisa789 <yayalookwha@gmail.com>
2026-08-13 10:23:49 +0700

    .....

---
+++ lecture6/two-dimen.py
@@ -0,0 +1,10 @@
+import random
+ROWS = 3
+COLS = 4
+def main():
+    values = [[0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    for r in range(ROWS):
+        for c in range(COLS):
+            values[r][c] = random.radint(1,100)
+    print(values)
+main()
