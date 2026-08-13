39c3b3998d3ff3d47c5e0bca95f01adac9f6a32a
yanisa789 <yayalookwha@gmail.com>
2026-08-13 09:36:31 +0700

    .....

---
+++ lecture6/slicing.py
@@ -0,0 +1,3 @@
+data = list(range(100))
+sliced_data = data[10:51:5]
+print(f"Sliced data: {sliced_data}")
---
+++ lecture6/slicing2.py
@@ -0,0 +1,5 @@
+numbers = [0,1,2,3,4,5,6,7,8,9]
+print(numbers[2:6])
+print(numbers[1:8:2])
+print(numbers[:4])
+print(numbers[6:])
